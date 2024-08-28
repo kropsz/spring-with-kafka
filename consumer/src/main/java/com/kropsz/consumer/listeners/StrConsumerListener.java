802bf5e29010faa927994299e9e7015957dcf8b0
Pedro Esteves <kropinho211@outlook.com>
2024-08-28 10:24:19 -0300

    Determinando partição para um Consumer especifico

--- consumer/src/main/java/com/kropsz/consumer/listeners/StrConsumerListener.java
+++ consumer/src/main/java/com/kropsz/consumer/listeners/StrConsumerListener.java
@@ -1,6 +1,7 @@
 package com.kropsz.consumer.listeners;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
@@ -9,10 +10,17 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class StrConsumerListener {
 
-    @KafkaListener(topics = "str-topic",
-                   groupId = "group-1",
-                   containerFactory = "stringKafkaListenerContainerFactory")
-    public void listner(String message) {
+    @KafkaListener(topics = "str-topic", groupId = "group-1",
+    topicPartitions = {
+        @TopicPartition(topic = "str-topic", partitions = { "0" }),
+       }
+    ,containerFactory = "stringKafkaListenerContainerFactory")
+    public void create(String message) {
+        log.info("Message Receive: {}", message);
+    }
+
+    @KafkaListener(topics = "str-topic", groupId = "group-1", containerFactory = "stringKafkaListenerContainerFactory")
+    public void log(String message) {
         log.info("Message Receive: {}", message);
     }
 }
