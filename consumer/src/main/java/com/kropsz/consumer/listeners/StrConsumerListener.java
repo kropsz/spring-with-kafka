a2bebf8bb9ae4286a0083ef40b9d29fb09291117
Pedro Esteves <kropinho211@outlook.com>
2024-08-28 10:34:01 -0300

    Implementando um listener customizado

--- consumer/src/main/java/com/kropsz/consumer/listeners/StrConsumerListener.java
+++ consumer/src/main/java/com/kropsz/consumer/listeners/StrConsumerListener.java
@@ -1,8 +1,8 @@
 package com.kropsz.consumer.listeners;
 
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
+
+import com.kropsz.consumer.custom.StrCustomConsumerListener;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,16 +10,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class StrConsumerListener {
 
-    @KafkaListener(topics = "str-topic", groupId = "group-1",
-    topicPartitions = {
-        @TopicPartition(topic = "str-topic", partitions = { "0" }),
-       }
-    ,containerFactory = "stringKafkaListenerContainerFactory")
+    @StrCustomConsumerListener(groupId = "group-1")
     public void create(String message) {
         log.info("Message Receive: {}", message);
     }
 
-    @KafkaListener(topics = "str-topic", groupId = "group-1", containerFactory = "stringKafkaListenerContainerFactory")
+    @StrCustomConsumerListener(groupId = "group-1")
     public void log(String message) {
         log.info("Message Receive: {}", message);
     }
